7feb34f34afd093e713cbf916f9d91fdf9401c2d
rutujakesare <rutujakesare38@gmail.com>
2023-11-06 12:11:41 +0530

    Main

---
+++ main.js
@@ -0,0 +1,65 @@
+const candyList = document.getElementById("candyList");
+const addCandyButton = document.getElementById("addCandy");
+
+let candies = [];
+
+function addCandyToCrudCrud(candyData) {
+
+    candies.push(candyData);
+
+  
+    const listItem = document.createElement("li");
+    candyList.appendChild(listItem);
+    
+    function updateListItem() {
+        listItem.innerHTML = `${candyData.name} - ${candyData.description}, Price: Rs.${candyData.price}, Quantity: ${candyData.quantity}
+        <button class="buy" data-amount="1">Buy One</button>
+        <button class="buy" data-amount="2">Buy Two</button>
+        <button class="buy" data-amount="3">Buy Three</button>`;
+        
+        const buyButtons = listItem.getElementsByClassName("buy");
+        for (const button of buyButtons) {
+            button.addEventListener("click", function () {
+                const amount = parseInt(button.getAttribute("data-amount"));
+                candyData.quantity -= amount;
+                if (candyData.quantity < 0) {
+                    candyData.quantity = 0;
+                }
+                updateListItem(); 
+            });
+        }
+    }
+
+    updateListItem(); 
+
+   
+    document.getElementById("candyName").value = "";
+    document.getElementById("candyDescription").value = "";
+    document.getElementById("candyPrice").value = "";
+    document.getElementById("candyQuantity").value = "";
+
+   
+    
+
+}
+
+addCandyButton.addEventListener("click", function () {
+    const candyName = document.getElementById("candyName").value;
+    const candyDescription = document.getElementById("candyDescription").value;
+    const candyPrice = parseFloat(document.getElementById("candyPrice").value);
+    const candyQuantity = parseInt(document.getElementById("candyQuantity").value);
+
+    if (candyName && candyPrice >= 0 && candyQuantity >= 0) {
+        const candyData = {
+            name: candyName,
+            description: candyDescription,
+            price: candyPrice,
+            quantity: candyQuantity,
+        };
+
+        addCandyToCrudCrud(candyData);
+    }
+});
+
+
+
